aeabd7907fbdd4f20a09be603a8faf225ecb50ff
Bin Xia <bxia@cs.fiu.edu>
2017-12-31 14:38:27 +0800

    Fix async problem

--- vendor/resource/common.js
+++ vendor/resource/common.js
@@ -1,4 +1,50 @@
+
+
+
+
+
+
+
 var localSign = 0;
+
+
+var speakers = [];
+for (var name in members) {
+  speakers.push({"name":name});
+}
+
+
+var CountCombobox,
+    CountPara = {
+      "id":"SupplementCount",
+      "renderer":"value",
+      "valueField":"value",
+      "sortOrder":"Id",
+      "data":[
+          {"Id":1,"value":1},
+          {"Id":2,"value":2},
+          {"Id":3,"value":3},
+          {"Id":4,"value":4},
+          {"Id":5,"value":5},
+          {"Id":6,"value":6},
+          {"Id":7,"value":7},
+          {"Id":8,"value":8},
+          {"Id":9,"value":9}
+      ]
+    },
+    SpeakCombobox,
+    SpeakerPara = {
+      "id":"Speaker",
+      "renderer":"name",
+      "valueField":"name",
+      "data":speakers
+    };
+
+
+
+
+
+
 
 // Bind click function
 function initListListeners(ListID,List){    
@@ -39,7 +85,6 @@ function initListListeners(ListID,List){
 
 
 
-
 function creSupplementList(Parent,id) {
   var table = $("<table>",{id:id||"List",class:"display",cellspacing:"0",width:"100%"});
   var thead = $("<thead>").append($("<tr>",{id:(id?"SubSearch":"Search")})
@@ -50,9 +95,35 @@ function creSupplementList(Parent,id) {
   return Parent;
 }
 
-function creAddPresentation(Parent,id){
-  
 
+function creAddPresentation(Parent,id){
+  var label = $("<label>",{class:"col-sm-3",align:"right"}).append("Meeting Date: &nbsp;");
+  var input = $("<input>",{class:"col-sm-2",type:"text",id:"MeetingDate",readonly:true});
+  var DateDIV = $("<div>",{class:"col-sm-12"});
+  DateDIV.append(label).append(input);
+  var label = $("<label>",{class:"col-sm-3",align:"right",style:"float:none;vertical-align:middle;display:inline-block;margin-top:-10px"}).append("Speaker: &nbsp;");
+  var combobox = $("<div>",{class:"col-sm-3",style:"display:inline-block;margin-left:-15px"}).append($("<div>",{id:"Speaker"}));
+  var SpeakerDIV = $("<div>",{class:"col-sm-12",style:"white-space:nowrap;margin-bottom:5px"});
+  SpeakerDIV.append(label).append(combobox);
+  // var label = $("<label>",{class:"col-sm-3",align:"right",style:"float:none;vertical-align:middle;display:inline-block;margin-top:-10px"}).append("Category: &nbsp;");
+  // var combobox = $("<div>",{class:"col-sm-3",style:"display:inline-block;margin-left:-15px"}).append($("<div>",{id:"Category"}));
+  // var CategoryDIV = $("<div>",{class:"col-sm-12",style:"white-space:nowrap;margin-bottom:5px"});
+  // CategoryDIV.append(label).append(combobox);
+  var label = $("<label>",{class:"col-sm-3",align:"right"}).append("Title: &nbsp;");
+  var input = $("<input>",{class:"col-sm-9 range",type:"text",id:"Title"});
+  var TitleDIV = $("<div>",{class:"col-sm-12"});
+  TitleDIV.append(label).append(input);
+  var label = $("<label>",{class:"col-sm-3",align:"right"}).append("URL: &nbsp;");
+  var input = $("<input>",{class:"col-sm-9 range",type:"text",id:"TitleURL"});
+  var TitleURLDIV = $("<div>",{class:"col-sm-12"});
+  TitleURLDIV.append(label).append(input);
+  var label = $("<label>",{class:"col-sm-3",align:"right",style:"float:none;vertical-align:middle;display:inline-block;margin-top:-10px"}).append("Supplement: &nbsp;");
+  var combobox = $("<div>",{class:"col-sm-3",style:"display:inline-block;margin-left:-15px"}).append($("<div>",{id:"SupplementCount"}));
+  var CountDIV = $("<div>",{class:"col-sm-12",style:"white-space:nowrap"});
+  CountDIV.append(label).append(combobox);
+  var SupplementDIV = $("<div>",{class:"col-sm-12"});
+
+  Parent.append(DateDIV).append(SpeakerDIV).append(TitleDIV).append(TitleURLDIV).append(CountDIV).append(SupplementDIV);
   return Parent;
 }
 
@@ -70,6 +141,119 @@ function creAddPresentation(Parent,id){
 
 
 
+
+
+
+// 初始化信息列表
+function initList(List,para){
+  if (List!=null) {
+    List.destroy();
+  }
+  var List = $('#'+para.id).DataTable({
+    data: para.presentations,
+    columns: [
+      { data: 0, title: "Date"},
+      { data: 1, title: "Category", className: "dt-center",},
+      { data: 2, title: "Title", width: "40%",
+        fnCreatedCell: function (nTd, sData, oData, iRow, iCol) {
+          if (localSign == 0) {
+            $(nTd).html("<a href="+para.papers[sData]["PDF"]+" target=\"view_window\">"+sData+"</a>");
+          }
+          else {
+            $(nTd).html("<a href="+para.papers[sData]["PDF_Local"]+" target=\"view_window\">"+sData+"</a>");
+          }
+        }
+      },
+      { data: 3, title: "Speaker", className: "dt-center",
+        fnCreatedCell: function (nTd, sData, oData, iRow, iCol) {
+          if (members[sData].hasOwnProperty("Homepage")) {
+            $(nTd).html("<a href="+members[sData]["Homepage"]+">"+sData+"</a>")
+          }
+          else {
+            $(nTd).html(sData)
+          }
+        }
+      },
+      { data: 4, title: "Supplement", width: "10%", className: "dt-center"},
+    ],
+    dom: 'Bfrtip',
+    buttons: [
+      {
+        text: 'Add',
+        action: function () {
+          var paraModal = {
+            "id":"Modal",
+            "size":"modal-dialog modal-lg",
+            "title":"Add new presentation",
+            "parent":$("#"+para.shell),
+            "subject":[
+              creAddPresentation($("<div>",{class:"col-sm-12 margin",style:"margin-top:10px"}),"SubList")
+            ],
+            "buttonID":"AddPresentation",
+            "button":"Add",
+            "EVENT":
+              function AddPresentation(){
+                
+              }
+          };
+          initModal(paraModal);
+          MeetingDate = initMeetingDate();
+          CountCombobox = initCombobox(CountPara);
+          SpeakerCombobox = initCombobox(SpeakerPara);
+        }
+      },
+      {
+        text: 'Details',
+        action: function () {
+          if (List.$('tr.selected').length != 0) {
+            var paraModal = {
+              "id":"Modal",
+              "size":"modal-dialog modal-lg",
+              "title":List.rows('.selected').data()[0][2],
+              "parent":$("#"+para.shell),
+              "subject":[
+                creSupplementList($("<div>"),"SubList")
+              ],
+              "backdrop":true,
+              "buttonID":"Close",
+              "button":"Close",
+              "EVENT":
+                function Close(){
+                  $("#closeSubModal").click();
+                }
+            };
+            initModal(paraModal);
+            initModalList({"Supplement":List.rows('.selected').data()[0][2]},"SubList");
+          }
+          else {
+            alert("Please select a presentation!")
+          }
+        }
+      }
+    ],
+    order: [[ 0, "desc" ]],
+  });
+
+  return List;
+}
+
+
+// Meeting Date
+function initMeetingDate(){
+  var MeetingDate = $("#MeetingDate").datepicker({
+    // showButtonPanel: true,
+    constrainInput: true,
+    // changeYear: true,
+    // changeMonth: true,
+    showOn: "both",
+    // buttonImage: "img/calendar.gif",
+    // buttonImageOnly: false,
+    defaultDate: null,
+    dateFormat: "yy/mm/dd" 
+  })
+  MeetingDate.datepicker('setDate', new Date());
+  return MeetingDate;
+}
 
 
 function initModalList(listType,id){
@@ -161,65 +345,66 @@ function initModal(para){
   })
 }
 
+
 // 初始化Combobox控件
 function initCombobox(para){
-  var Combobox = $('#'+para.id).magicSuggest({
-      renderer: function(data){
-          if (para.hasOwnProperty("renderer")) {
-              var field = para.renderer.split("+");
-              var string = "";
-              for (var i = 0; i < field.length; i++) {
-                  // join field and string using "+"
-                  string += (data.hasOwnProperty(field[i]))?data[field[i]].toString():field[i].toString();
-              }
-              return string;
-          }
-          return data.name;
-          
-      },
-      selectionRenderer: function(data){
-          if (para.hasOwnProperty("selection")) {
-              return data[para.selection];
-          }
-          else {
-              var field = para.renderer.split("+");
-              var string = "";
-              for (var i = 0; i < field.length; i++) {
-                  // join field and string using "+"
-                  string += (data.hasOwnProperty(field[i]))?data[field[i]].toString():field[i].toString();
-              }
-              return string;
-          }
-      },
-      valueField: (para.hasOwnProperty("valueField"))?para.valueField:"id",
-      toggleOnClick: (para.hasOwnProperty("toggleOnClick"))?para.toggleOnClick:true,
-      resultAsString: (para.hasOwnProperty("resultAsString"))?para.resultAsString:true,
-      sortOrder: (para.hasOwnProperty("sortOrder"))?para.sortOrder:(para.hasOwnProperty("valueField"))?para.valueField:"id",
-      hideTrigger: (para.hasOwnProperty("hideTrigger"))?para.hideTrigger:false,
-      maxDropHeight: (para.hasOwnProperty("maxDropHeight"))?para.maxDropHeight:145,
-      autoSelect: (para.hasOwnProperty("autoSelect"))?para.autoSelect:true,
-      value: (para.hasOwnProperty("value"))?para.value:[],
-      allowFreeEntries: (para.hasOwnProperty("allowFreeEntries"))?para.allowFreeEntries:false,
-      useTabKey: true,
-      useZebraStyle: true,
-      matchCase: true,
-      maxSelection: (para.hasOwnProperty("maxSelection"))?para.maxSelection:1,
-      disabled: (para.hasOwnProperty("disabled"))?para.disabled:false,
-      placeholder: (para.hasOwnProperty("placeholder"))?para.placeholder:" ",
-      noSuggestionText: (para.hasOwnProperty("noSuggestionText"))?para.noSuggestionText:'未找到 {{query}}'
-  });
-  
-  if (para.hasOwnProperty("url")) {
-      $.getJSON(para.url,function(data){
-          Combobox.setData(data);
-          Combobox.setValue((para.hasOwnProperty("setValue"))?para.setValue:[]);
-      })
-  }
-  else if (para.hasOwnProperty("data")) {
-      Combobox.setData(para.data);
-  }
-  else {
-      throw("No Data!");
-  }
-  return Combobox;
+    var Combobox = $('#'+para.id).magicSuggest({
+        renderer: function(data){
+            if (para.hasOwnProperty("renderer")) {
+                var field = para.renderer.split("+");
+                var string = "";
+                for (var i = 0; i < field.length; i++) {
+                    // join field and string using "+"
+                    string += (data.hasOwnProperty(field[i]))?data[field[i]].toString():field[i].toString();
+                }
+                return string;
+            }
+            return data.name;
+            
+        },
+        selectionRenderer: function(data){
+            if (para.hasOwnProperty("selection")) {
+                return data[para.selection];
+            }
+            else {
+                var field = para.renderer.split("+");
+                var string = "";
+                for (var i = 0; i < field.length; i++) {
+                    // join field and string using "+"
+                    string += (data.hasOwnProperty(field[i]))?data[field[i]].toString():field[i].toString();
+                }
+                return string;
+            }
+        },
+        valueField: (para.hasOwnProperty("valueField"))?para.valueField:"id",
+        toggleOnClick: (para.hasOwnProperty("toggleOnClick"))?para.toggleOnClick:true,
+        resultAsString: (para.hasOwnProperty("resultAsString"))?para.resultAsString:true,
+        sortOrder: (para.hasOwnProperty("sortOrder"))?para.sortOrder:(para.hasOwnProperty("valueField"))?para.valueField:"id",
+        hideTrigger: (para.hasOwnProperty("hideTrigger"))?para.hideTrigger:false,
+        maxDropHeight: (para.hasOwnProperty("maxDropHeight"))?para.maxDropHeight:145,
+        autoSelect: (para.hasOwnProperty("autoSelect"))?para.autoSelect:true,
+        value: (para.hasOwnProperty("value"))?para.value:[],
+        allowFreeEntries: (para.hasOwnProperty("allowFreeEntries"))?para.allowFreeEntries:false,
+        useTabKey: true,
+        useZebraStyle: true,
+        matchCase: true,
+        maxSelection: (para.hasOwnProperty("maxSelection"))?para.maxSelection:1,
+        disabled: (para.hasOwnProperty("disabled"))?para.disabled:false,
+        placeholder: (para.hasOwnProperty("placeholder"))?para.placeholder:" ",
+        noSuggestionText: (para.hasOwnProperty("noSuggestionText"))?para.noSuggestionText:'未找到 {{query}}'
+    });
+    
+    if (para.hasOwnProperty("url")) {
+        $.getJSON(para.url,function(data){
+            Combobox.setData(data);
+            Combobox.setValue((para.hasOwnProperty("setValue"))?para.setValue:[]);
+        })
+    }
+    else if (para.hasOwnProperty("data")) {
+        Combobox.setData(para.data);
+    }
+    else {
+        throw("No Data!");
+    }
+    return Combobox;
 }
--- vendor/resource/info.js
+++ vendor/resource/info.js
@@ -1,94 +1,27 @@
 // Personal Information
 var members = {
-	"Linqing Liu":{
+    "Linqing Liu":{
 
-	},
-	"Shuo Wang":{
+    },
+    "Shuo Wang":{
 
-	},
-	"Keji Han":{
+    },
+    "Keji Han":{
 
-	},
-	"Jie Hang":{
+    },
+    "Jie Hang":{
 
-	},
+    },
+    "Ziye Zhu":{
+
+    },
+    "Yu Wang":{
+
+    },
+    "Yue Huang":{
+
+    },
+    "Bin Xia":{
+        "Homepage":"https://binxia.github.io/"
+    }
 }
-
-
-var papers = [];
-var presentations = [];
-
-$.getJSON("vendor/resource/presentation.json", function(data){
-	presentations = data["data"];
-})
-
-$.getJSON("vendor/resource/paper.json", function(data){
-	papers = data;
-})
-
-
-// Paper Information
-// var papers = {
-// 	"SeqGAN: Sequence Generative Adversarial Nets with Policy Gradient":{
-// 		"PDF":"http://www.aaai.org/ocs/index.php/AAAI/AAAI17/paper/download/14344/14489",
-// 		"PDF_Local":"resource/2017-12/14344-66977-1-PB.pdf",
-// 		"Supplement":{
-// 			"2017-02-07-aaai-seqgan.pdf":{
-// 				"Type":"PPT",
-// 				"URL":"http://lantaoyu.github.io/files/2017-02-07-aaai-seqgan.pdf",
-// 				"URL_Local":"resource/2017-12/2017-02-07-aaai-seqgan.pdf",
-// 			},
-// 		}
-// 	},
-// 	"STRICT: Information Retrieval Based Search Term Identification for Concept Location":{
-// 		"PDF":"http://ieeexplore.ieee.org/abstract/document/7884611",
-// 		"PDF_Local":"resource/2017-12/Information Retrieval Based Search Term.pdf",
-// 		"Supplement":{
-// 			"STRICT-SANER2017.pptx":{
-// 				"Type":"PPT",
-// 				"URL_Local":"resource/2017-12/STRICT-SANER2017.pptx",
-// 			},
-// 			"2017-12-30 representation.pptx":{
-// 				"Type":"PPT",
-// 				"URL":"resource/2017-12/2017-12-30-representation.pptx",
-// 				"URL_Local":"resource/2017-12/2017-12-30-representation.pptx",
-// 			},
-// 		}
-// 	},
-
-// 	"In Defense of Fully Connected Layers in Visual Representation Transfer":{
-// 		"PDF":"https://pdfs.semanticscholar.org/4642/15c3c882596b028e98aa05b88ce30c0e9496.pdf",
-// 		"PDF_Local":"resource/2017-12/PCM2017_FC.pdf",
-// 		"Supplement":{
-// 			"12.23周末讨论.pptx":{
-// 				"Type":"PPT",
-// 				"URL":"resource/2017-12/12.23周末讨论.pptx",
-// 				"URL_Local":"resource/2017-12/12.23周末讨论.pptx",
-// 			}
-// 		}
-// 	},
-
-// 	"Generating Adversarial Malware Examples for Black-Box Attacks Based on GAN":{
-// 		"PDF":"https://arxiv.org/abs/1702.05983",
-// 		"PDF_Local":"MalGAN_IJCAI_2017_Hu_Tan.pdf",
-// 		"Supplement":{
-// 			"Generating Adversarial Malware Examples for Black-Box Attacks Based on GAN.pptx":{
-// 				"Type":"PPT",
-// 				"URL":"resource/2017-12/Generating Adversarial Malware Examples for Black-Box Attacks Based on GAN.pptx",
-// 				"URL_Local":"resource/2017-12/Generating Adversarial Malware Examples for Black-Box Attacks Based on GAN.pptx",
-// 			}
-// 		}
-// 	},
-
-// 	"":{
-// 		"PDF":"",
-// 		"PDF_Local":"",
-// 		"Supplement":{
-// 			"":{
-// 				"Type":"",
-// 				"URL":"",
-// 				"URL_Local":"",
-// 			}
-// 		}
-// 	},
-// }
